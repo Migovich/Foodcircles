8bc36a0851fc7f834f07912bb4276500d5141a1e
David Groulx <david@sandbendersoftware.com>
2013-05-01 10:45:18 -0400

    Initial Commit

---
+++ FoodCircles/FCSAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  FCSAppDelegate.h
+//  FoodCircles
+//
+//  Created by David Groulx on 5/1/13.
+//  Copyright (c) 2013 FoodCircles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ FoodCirclesTests/FoodCirclesTests.m
@@ -0,0 +1,32 @@
+//
+//  FoodCirclesTests.m
+//  FoodCirclesTests
+//
+//  Created by David Groulx on 5/1/13.
+//  Copyright (c) 2013 FoodCircles. All rights reserved.
+//
+
+#import "FoodCirclesTests.h"
+
+@implementation FoodCirclesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FoodCirclesTests");
+}
+
+@end
