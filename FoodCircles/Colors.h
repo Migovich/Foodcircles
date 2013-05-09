cfba81f306a5ee075023163f30729273427d0e34
David Groulx <david@sandbendersoftware.com>
2013-05-09 11:13:55 -0400

    Xcode got confused

--- FoodCircles/Colors.h
+++
@@ -1,9 +0,0 @@
-//
-//  Colors.h
-//  FoodCircles
-//
-//  Created by David Groulx on 5/6/13.
-//  Copyright (c) 2013 FoodCircles. All rights reserved.
-//
-
-const UIColor *background = [UICOlor colorWith]
