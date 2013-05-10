141ea9eb9bb7faba945c841f1c9a74e95dcf1c40
David Groulx <david@sandbendersoftware.com>
2013-05-10 11:01:41 -0400

    whitespace issues

--- FoodCircles/FCSCopyLabel.m
+++ FoodCircles/FCSCopyLabel.m
@@ -13,20 +13,20 @@
 
 - (id)initWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-      self.textColor = [FCSStyles primaryTextColor];
-    }
-    return self;
+  self = [super initWithFrame:frame];
+  if (self) {
+    self.textColor = [FCSStyles primaryTextColor];
+  }
+  return self;
 }
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect
+ {
+ // Drawing code
+ }
+ */
 
 @end
--- FoodCircles/FCSHeaderLabel.m
+++ FoodCircles/FCSHeaderLabel.m
@@ -13,20 +13,20 @@
 
 - (id)initWithCoder:(NSCoder *)aDecoder {
   self = [super initWithCoder:aDecoder];
-    if (self) {
-      self.textColor = [FCSStyles primaryTextColor];
-      self.font = [UIFont systemFontOfSize:22];
-    }
-    return self;
+  if (self) {
+    self.textColor = [FCSStyles primaryTextColor];
+    self.font = [UIFont systemFontOfSize:22];
+  }
+  return self;
 }
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect
+ {
+ // Drawing code
+ }
+ */
 
 @end
