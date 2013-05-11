05c96c05f1483e4a31bf62cc2e02c46f28eb1b73
David Groulx <david@sandbendersoftware.com>
2013-05-11 11:47:33 -0400

    More styles

---
+++ FoodCircles/FCSCodeLabel.m
@@ -0,0 +1,31 @@
+//
+//  FCSCodeLabel.m
+//  FoodCircles
+//
+//  Created by David Groulx on 5/10/13.
+//  Copyright (c) 2013 FoodCircles. All rights reserved.
+//
+
+#import "FCSCodeLabel.h"
+
+@implementation FCSCodeLabel
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
