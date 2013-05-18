38cbc12466b85b4e48d83bd9e7e568261b42b2e2
David Groulx <david@sandbendersoftware.com>
2013-05-18 10:20:47 -0400

    Image scroll on the landing page is functioning.

--- FoodCircles/FCSLandingPageViewController.m
+++ FoodCircles/FCSLandingPageViewController.m
@@ -9,6 +9,10 @@
 #import "FCSLandingPageViewController.h"
 #import "FCSRemoteAPI.h"
 
+const CGFloat CAROUSEL_WIDTH = 320.0; // pixels
+const CGFloat CAROUSEL_HEIGHT = 172.0; // pixels
+const int CAROUSEL_DELAY = 5; // seconds
+
 @interface FCSLandingPageViewController ()
 
 @property NSArray *carouselImageArray;
@@ -26,6 +30,37 @@
   [super viewDidLoad];
   
   carouselImageArray = [FCSRemoteAPI landingImages];
+
+  // Manually set the content size of the scroll view. This is necessary with autolayout since
+  // constraints are not loaded until later
+//  carouselView.contentSize = CGSizeMake(CAROUSEL_WIDTH * carouselImageArray.count, CAROUSEL_HEIGHT);
+
+  // Initialize the scrollview's content
+  [carouselImageArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:carouselImageArray[idx]];
+    imageView.clipsToBounds = YES;
+    imageView.contentMode = UIViewContentModeScaleAspectFit;
+    // Manually set  image view sizes as well to force the scroll view to expand to hold the images
+    imageView.frame = CGRectMake(CAROUSEL_WIDTH * idx, 0, CAROUSEL_WIDTH, CAROUSEL_HEIGHT);
+    [carouselView addSubview:imageView];
+  }];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+  
+  [NSTimer scheduledTimerWithTimeInterval:CAROUSEL_DELAY target:self selector:@selector(advanceTheScroller:) userInfo:nil repeats:YES];
+}
+
+- (void)advanceTheScroller:(NSTimer *)timer {
+  CGPoint offset = carouselView.contentOffset;
+  // Determine if scroll has reached the end
+  if (offset.x >= CAROUSEL_WIDTH * (carouselImageArray.count - 1)) { // loop back to the start
+    offset = CGPointZero;
+  } else {
+    offset.x += CAROUSEL_WIDTH;
+  }
+  [carouselView setContentOffset:offset animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,12 +75,7 @@
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
   if ([segue.identifier isEqualToString:@"showVenueList"]) {
-//    NSIndexPath *indexPath = [[self.collectionView indexPathsForSelectedItems] objectAtIndex:0];
-//    FCSVenue *venue = [self.fetchedResultsController objectAtIndexPath:indexPath];
-//    FCSVenueViewController *destinationViewController = (FCSVenueViewController *)segue.destinationViewController;
-//    destinationViewController.venue = venue;
-//    destinationViewController.title = venue.name;
-    NSLog(@"preparing segue showVenueList");
   }
 }
+
 @end
