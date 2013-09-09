7e6c14f4aa759562b0f4be96e00aaee44d5b8a41
Simon Gislen <simongislen@yahoo.in>
2013-09-09 21:50:41 +0530

    Added SDWebImage

---
+++ FoodCircles/Cells/FCSMultilineCell.h
@@ -0,0 +1,13 @@
+//
+//  FCSMultilineCell.h
+//  FoodCircles
+//
+//  Created by Simon Gislen on 09/09/13.
+//  Copyright (c) 2013 FoodCircles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCSMultilineCell : UITableViewCell
+
+@end
